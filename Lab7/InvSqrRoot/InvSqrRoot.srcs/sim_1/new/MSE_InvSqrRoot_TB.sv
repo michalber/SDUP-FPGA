313f625b8f91824bd34e5ed54d856b83549a1a06
michalber <michalberdzik97@gmail.com>
2021-05-24 12:29:24 +0200

    Update MSE Testbench

--- Lab7/InvSqrRoot/InvSqrRoot.srcs/sim_1/new/MSE_InvSqrRoot_TB.sv
+++ Lab7/InvSqrRoot/InvSqrRoot.srcs/sim_1/new/MSE_InvSqrRoot_TB.sv
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+`timescale 100ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -23,29 +23,24 @@
 module MSE_InvSqrRoot_TB();
 
 function real inv_sqr_root (input real x);
-  if (x > 0.25) // cos is easier to calculate
-     sin= cos( 1.570796327 - x); // sin(x) = cos(90 - x)
-  else begin // Taylor
-    real x2= x*x; 
-    sin= x * (1.0 + x2 * ( -1.0 / 6.0 + x2 * ( 1.0 / 120.0 - x2 / 5040.0) ) ); // Taylor series skiped x^9 / 9!
-    end 
+    inv_sqr_root = 1.0 / $sqrt(x);
 endfunction
 
 reg clock, reset, start=1'b1;
-reg [11:0] val_in= 0;
+reg [24:0] val_in= 52428;
 wire ready_out;
 reg ready_out_q;
-wire signed [11:0] sin_out;
+wire signed [24:0] val_out;
 int N= 0; // number of test iterations 
-real real_angle, real_sin, real_sin_correct, error_sin_LSB;
+real real_x, real_out, real_out_correct, error_out_LSB;
 real error_max_LSB= 0.0,  abs; // maximum error of sine and cosine
-real sum_error2_sin_LSB= 0.0; // sum of square errors for sin and cos (in LSB)
-real sum_error_sin_LSB= 0.0; // sum of errrors
-real MSE_sin_LSB; // Mean Square Error: MSE= SUM(error^2) / N (N- number of sum elements)
-real ME_sin_LSB; // Mean Error for sin and cos
-const real FIX_1 = 1 / 1024.0; // fix point representation of the LSBit.
+real sum_error2_out_LSB= 0.0; // sum of square errors for sin and cos (in LSB)
+real sum_error_out_LSB= 0.0; // sum of errrors
+real MSE_out_LSB; // Mean Square Error: MSE= SUM(error^2) / N (N- number of sum elements)
+real ME_out_LSB; // Mean Error for sin and cos
+const real FIX_1 = 1 / 2**19; // fix point representation of the LSBit.
 
-NRInvSqrRootSM UnitUnderTest( clock, reset, start, val_in, ready_out, sin_out);
+NRInvSqrRootSM UnitUnderTest( clock, reset, start, val_in, ready_out, val_out);
 
 //Clock generator
 initial
@@ -66,27 +61,27 @@ end
 
 always_ff @ (posedge clock) begin
   if (ready_out_q == 0 && ready_out == 1) begin // ready_out is active - rising
-    N+= 1;
-    real_angle= val_in * FIX_1;
-    real_sin= sin_out * FIX_1;
+    N += 1;
+    real_x= $bitstoreal(val_in) / 2**19;
+    real_out= $bitstoreal(val_out) / 2**19;
     // compare results with the real value and calculate the error and maximum error
-    real_sin_correct= sin(real_angle);
-    error_sin_LSB= (real_sin_correct - real_sin) / FIX_1;
-    sum_error2_sin_LSB+= error_sin_LSB * error_sin_LSB;
-    sum_error_sin_LSB+= error_sin_LSB;
-    MSE_sin_LSB= sum_error2_sin_LSB / N;
-    ME_sin_LSB= sum_error_sin_LSB / N;
-    abs= error_sin_LSB;
+    real_out_correct= inv_sqr_root(real_x);
+    error_out_LSB= (real_out_correct - real_out) / 2**19;
+    sum_error2_out_LSB+= error_out_LSB * error_out_LSB;
+    sum_error_out_LSB+= error_out_LSB;
+    MSE_out_LSB = sum_error2_out_LSB / N;
+    ME_out_LSB= sum_error_out_LSB / N;
+    abs= error_out_LSB;
     if (abs < 0) abs= -abs;
     if ( abs > error_max_LSB) error_max_LSB= abs;
     
-    val_in<= val_in + 1;
-    if (val_in <= 1609 ) // angle Pi /2
+    val_in<= val_in + 52428;
+    if (val_in <= 3670016 )
       start <= 1;
     else
-      $stop; // stop simulation as Pi/2 has been reached - every angle was simulated
+      $stop;
     end
-  else if ($realtime > 100ns)
+  else if ($realtime > 1000ns)
     start<= 0;
 end
 
